ceeaf42db570d70c7026ee6367353f54e4df72e5
Tom Klein <twointofive@gmail.com>
2017-01-03 21:09:10 -0600

    Initial commit.

---
+++ dp.js
@@ -0,0 +1,478 @@
+"use strict";
+var dp = {};
+
+// the web worker that actually generates our data (see dpWorker.js)
+dp.worker = null;
+// one call to draw() produces one frame of output; we create draw()
+// (with initializeDrawer()) each time the control parameters change
+// so that the parameters are "baked in".
+dp.draw = function() {};
+// dp.draw is called through a timer - remember which one
+dp.drawTimerID = 0;
+// the number of data points we (typically) ask for on each call to
+// the worker
+dp.dataFetchSize = 520;
+
+// store and serve data for the simulation
+dp.dataStore = (function() {
+
+  //////// private
+  // Two element array: current data set (index specified by
+  // |curDataIndex|) and next data set (the other index).
+  // Each |data| item is stored as a string in the form
+  // theta_1-coord,theta_2-coord|theta_1-coord,theta_2-coord|...
+  // |lastPoint| includes velocity in addition to position (used to start
+  // the next data request) in the form
+  // theta_1,theta_2,omega_1,omega_2
+  var data = [{data: "", lastPoint: ""}, {data: "", lastPoint: ""}];
+  // specifies which index of data is currently in use
+  var curDataIndex = 0;
+  var nextDataIndex = function() {
+    return (curDataIndex + 1) % 2;
+  };
+  //////// public
+  return {
+    addData : function(newData, lastDataPoint) {
+    // always put new data at the inactive (non-drawing) data index
+    var newIndex = nextDataIndex();
+    data[newIndex].data = newData;
+    data[newIndex].lastPoint = lastDataPoint;
+    this.onDataLoaded();
+    },
+    getNextData : function() {
+      curDataIndex = nextDataIndex();
+      return data[curDataIndex].data;
+    },
+    getCurLastPoint : function() {
+      return data[curDataIndex].lastPoint;
+    },
+    // function called when new data is loaded - set/reset it as needed
+    onDataLoaded : function() {}
+  };
+}());
+
+//////////////////////////////////////////////////////////////////////
+
+// Creates a dp.draw function given the lengths l1 and l2 of the two
+// arms of the pendulum
+dp.initializeDrawer = function(l1, l2) {
+
+  // the actual list of coordinates to be drawn gets filled in
+  // the first time draw() is called (see the else leg below)
+  var coords = [];
+  var dataLength = coords.length;
+  var dataLengthOver2 = parseInt(dataLength / 2, 10);
+  var i = 0;
+  // drawer constants
+  var line1 = document.getElementById('line1');
+  var line2 = document.getElementById('line2');
+  var trail = document.getElementById('trail');
+  var sin = Math.sin;
+  var cos = Math.cos;
+  var PI = Math.PI;
+  dp.draw = function() {
+
+    if (i < dataLength) {
+      //// (on long runs there is an appreciable difference between using
+      //// split and using indexOf/substring, at least on ff...)
+      //var angles = coords[i].split(',');
+      //var theta1 = angles[0];
+      //var theta2 = angles[1];
+      var theseCoords = coords[i];
+      var comma = theseCoords.indexOf(',');
+      var theta1 = theseCoords.substring(0, comma);
+      var theta2 = theseCoords.substring(comma + 1);
+      // coordinates of the first pendulum
+      var x1 = l1 * sin(theta1);
+      var y1 = -l1 * cos(theta1);
+      // coordinates of the second pendulum
+      var x2 = x1 + l2 * sin(theta2);
+      var y2 = y1 - l2 * cos(theta2);
+      x1 = Math.round(x1 * 100) / 100;
+      x2 = Math.round(x2 * 100) / 100;
+      y1 = Math.round(y1 * 100) / 100;
+      y2 = Math.round(y2 * 100) / 100;
+      if (dp.trailOn) {
+        var trailData = trail.getAttributeNS(null, "points");
+        if (trailData.length > 1800) {
+          // remove the first point from the trail
+          var firstSpace = trailData.indexOf(' ');
+          trailData = trailData.substring(firstSpace + 1);
+        }
+        var separator = " ";
+        if (!trailData) {
+          // IE10 has undefined behavior if there's a space at the end
+          // of the list, so yeah, avoid that
+          // TODO: arrange things so that we can get rid of this check
+          // (need to know the point being drawn when the trail is turned
+          // on)
+          separator = "";
+        }
+        // append the new point to the trail
+        trailData = trailData + separator + x2 + "," + y2;
+        trail.setAttributeNS(null, "points", trailData);
+      }
+      line1.setAttributeNS(null, 'y2', y1);
+      line1.setAttributeNS(null, 'x2', x1);
+      line2.setAttributeNS(null, 'y2', y2);
+      line2.setAttributeNS(null, 'x2', x2);
+      line2.setAttributeNS(null, 'y1', y1);
+      line2.setAttributeNS(null, 'x1', x1);
+      if (dp.whichCat) {
+        var cat = document.getElementById(dp.whichCat);
+        cat.setAttributeNS(null, 'x', x2);
+        cat.setAttributeNS(null, 'y', y2);
+        var theta2Deg = theta2 * 180 / PI;
+        var rotate = "rotate(" + theta2Deg + " " + x2 + " " + y2 + ")";
+        cat.setAttributeNS(null, 'transform',
+                           rotate + " translate(-52,-47)");
+      }
+      i = i + 1;
+      if (i === dataLengthOver2) { // fetch the next data set
+        var lastPoint = dp.dataStore.getCurLastPoint();
+        dp.worker.postMessage("s" + dp.dataFetchSize + "!" + lastPoint);
+      }
+    }
+    else { // ran out of data - load us up
+      coords = dp.dataStore.getNextData().split('|');
+      dataLength = coords.length;
+      dataLengthOver2 = parseInt(dataLength / 2, 10);
+      i = 0;
+    }
+  };
+};
+
+// Very sad, but we better use degrees on the user side
+dp.fetchAngle = function(id) {
+  var value = document.getElementById(id).value;
+  return value * Math.PI / 180; // return radians
+};
+// note special behavior for fetching angles
+dp.fetchValue = function(id) {
+  if (id === "t1" || id === "t2" || id === "o1" || id === "o2") {
+    return dp.fetchAngle(id);
+  }
+  else {
+    return document.getElementById(id).value;
+  }
+};
+dp.setAngle = function(id, radValue) {
+  var degValue = radValue * 180 / Math.PI; // set to degrees
+  document.getElementById(id).value = degValue;
+};
+dp.setValue = function(id, value) {
+  if (id === "t1" || id === "t2" || id === "o1" || id === "o2") {
+    dp.setAngle(id, value);
+  }
+  else {
+    document.getElementById(id).value = value;
+  }
+};
+
+dp.getInitialConditions = function() {
+
+  var f = dp.fetchValue;
+  return [f('t1'), f('t2'), f('o1'), f('o2')];
+};
+
+dp.getParameters = function() {
+
+  var f = dp.fetchValue;
+  return [f('l1'), f('l2'), f('m1'), f('m2')];
+};
+
+// event handler for user clicking the Start/Pause button
+dp.startPause = function() {
+
+  var startPause = document.getElementById('startPause');
+  var curValue = startPause.value;
+  var newValue = "OOPS";
+  if (curValue === "Start") { // first start with cur control form values
+    newValue = "Pause";
+    // disable the button while we fetch the initial dataset
+    startPause.disabled = true;
+    // what to do when the first dataset arrives
+    dp.dataStore.onDataLoaded = (function() {
+      return function() {
+        dp.drawTimerID = setInterval(dp.draw, 25);
+        startPause.disabled = false;
+        dp.dataStore.onDataLoaded = (function() { return function() {};}());
+      };
+    }());
+    // on the first request, only fetch half the normal number of datapoints
+    var fetchSize = parseInt(dp.dataFetchSize / 2, 10);
+    dp.worker.postMessage("s" + fetchSize + "!" +
+                          dp.getInitialConditions().join('|'));
+  }
+  else if (curValue === "Continue") {
+    newValue = "Pause";
+    dp.drawTimerID = setInterval(dp.draw, 25); // restart drawing
+  }
+  else { // Pause
+    newValue = "Continue";
+    if (dp.drawTimerID) {
+      clearInterval(dp.drawTimerID);
+    }
+  }
+  startPause.value = newValue;
+};
+
+dp.initializeControlValues = function() {
+
+  var set = dp.setValue;
+  // check if previous settings have been saved in localStorage
+  if (localStorage && localStorage.getItem("l1")) {
+    set('l1', localStorage.getItem("l1"));
+    set('l2', localStorage.getItem("l2"));
+    set('m1', localStorage.getItem("m1"));
+    set('m2', localStorage.getItem("m2"));
+    set('t1', localStorage.getItem("t1"));
+    set('t2', localStorage.getItem("t2"));
+    set('o1', localStorage.getItem("o1"));
+    set('o2', localStorage.getItem("o2"));
+  }
+  else { // use defaults defined here
+    set('l1', '240');
+    set('l2', '100');
+    set('m1', '1');
+    set('m2', '1');
+    set('t1', '3.12413936106985'); // 179 degrees
+    set('t2', '3.12413936106985'); // 179 degrees
+    set('o1', '0');
+    set('o2', '0');
+  }
+};
+
+dp.storeConfigValues = function() {
+
+  if (localStorage) {
+    var f = dp.fetchValue;
+    localStorage.setItem("l1", f('l1'));
+    localStorage.setItem("l2", f('l2'));
+    localStorage.setItem("m1", f('m1'));
+    localStorage.setItem("m2", f('m2'));
+    localStorage.setItem("t1", f('t1'));
+    localStorage.setItem("t2", f('t2'));
+    localStorage.setItem("o1", f('o1'));
+    localStorage.setItem("o2", f('o2'));
+  }
+};
+
+dp.initializeWorkerAndDrawer = function() {
+
+  var configParameters = dp.getParameters();
+  dp.worker.postMessage("c" + configParameters.join('|'));
+  dp.initializeDrawer(configParameters[0], configParameters[1]);
+};
+
+dp.loadNewDrawer = function() {
+
+  // start by making sure the old drawer is stopped
+  if (dp.drawTimerID) {
+    clearInterval(dp.drawTimerID);
+  }
+  var goodNewValues = dp.checkControlValues();
+  var startPause = document.getElementById('startPause');
+  if (goodNewValues) {
+    startPause.disabled = true;
+    dp.initializeWorkerAndDrawer();
+    dp.drawInitialPendulum();
+    dp.storeConfigValues();
+    startPause.disabled = false;
+    startPause.value = "Start";
+  }
+  else {
+    startPause.value = "Continue";
+  }
+};
+
+dp.checkControlValues = function() {
+
+  var f = dp.fetchValue;
+  var l1 = f('l1');
+  var l2 = f('l2');
+  var m1 = f('m1');
+  var m2 = f('m2');
+  var t1 = f('t1');
+  var t2 = f('t2');
+  var o1 = f('o1');
+  var o2 = f('o2');
+  if (!dp.isNum(l1) || !dp.isNum(l2) || !dp.isNum(m1) || !dp.isNum(m2) ||
+      !dp.isNum(t1) || !dp.isNum(t2) || !dp.isNum(o1) || !dp.isNum(o2)) {
+    alert("All entries must be numbers.");
+    return false;
+  }
+  // TODO: getting NaN's when lengths or masses are too small - why?
+  if (l1 < 0.01 || l2 < 0.01) {
+    alert("Sorry, lengths must be at least .01.");
+    return false;
+  }
+  if (l1 > 1000 || l2 > 1000) {
+    alert("Sorry, lengths must be less than 1000.");
+    return false;
+  }
+  if (m1 < 0.01 || m2 < 0.01) {
+    alert("Sorry, masses must be at least .01.");
+    return false;
+  }
+  var twoPi = Math.PI * 2;
+  if (Math.abs(o1) > twoPi || Math.abs(o2) > twoPi) {
+    alert("Sorry, angular velocities can't be more than 360.");
+    return false;
+  }
+  return true;
+};
+
+dp.drawInitialPendulum = function() {
+
+  var inits = dp.getInitialConditions();
+  var theta1 = inits[0];
+  var theta2 = inits[1];
+  var params = dp.getParameters();
+  var l1 = params[0];
+  var l2 = params[1];
+  // coordinates of the end of the first pendulum
+  var x1 = l1 * Math.sin(theta1);
+  var y1 = -l1 * Math.cos(theta1);
+  // coordinates of the end of the second pendulum
+  var x2 = x1 + l2 * Math.sin(theta2);
+  var y2 = y1 - l2 * Math.cos(theta2);
+  x1 = Math.round(x1*100)/100;
+  x2 = Math.round(x2*100)/100;
+  y1 = Math.round(y1*100)/100;
+  y2 = Math.round(y2*100)/100;
+  var line1 = document.getElementById('line1');
+  var line2 = document.getElementById('line2');
+  line1.setAttributeNS(null, 'y2', y1);
+  line1.setAttributeNS(null, 'x2', x1);
+  line2.setAttributeNS(null, 'y2', y2);
+  line2.setAttributeNS(null, 'x2', x2);
+  line2.setAttributeNS(null, 'y1', y1);
+  line2.setAttributeNS(null, 'x1', x1);
+  var trail = document.getElementById('trail');
+  trail.setAttributeNS(null, 'points', "");
+  if (dp.whichCat) {
+    var cat = document.getElementById(dp.whichCat);
+    cat.setAttributeNS(null, 'x', x2);
+    cat.setAttributeNS(null, 'y', y2);
+    var theta2Deg = theta2 * 180/Math.PI;
+    var rotate = "rotate(" + theta2Deg + " " + x2 + " " + y2 + ")";
+    cat.setAttributeNS(null, 'transform',
+                       rotate + " translate(-52,-47)");
+  }
+};
+
+dp.setTrail = function() {
+
+  var checked = document.getElementById('trailCheckbox').checked;
+  if (checked) {
+    dp.trailOn = true;
+  }
+  else {
+    dp.trailOn = false;
+    var trail = document.getElementById('trail');
+    trail.setAttributeNS(null, "points", "");
+  }
+};
+
+// get textual SVG of the pendulua currently being drawn, and put it
+// in the "Get SVG" text box for the user to copy
+dp.generateSVG = function() {
+
+  var svgElement = document.getElementById('pendula');
+  var svgText = svgElement.innerHTML;
+  // do a little cleanup
+  svgText = svgText.replace(/^\n/, "");
+  svgText = svgText.replace(/\n$/, "");
+  svgText = svgText.replace(/ id="[\w\-]+"/g, "");
+  // if there isn't a trail then don't include the empty trail
+  svgText =
+    svgText.replace(/ *<polyline[="\w\s\-]*points=""><\/polyline>\n/,"");
+  document.getElementById('svgText').value = svgText;
+};
+
+dp.elementDisplayer = function(elementToDisplay) {
+
+  return function() { // switch the visibility of elementToDisplay
+    var element = document.getElementById(elementToDisplay);
+    var newVisibility = 'visible';
+    if (element.style.visibility === 'visible') {
+      newVisibility = 'hidden';
+    }
+    element.style.visibility = newVisibility;
+  };
+};
+
+dp.browserIsSupported = function() {
+
+  // test for support of web workers and svg
+  var browserIsSupported = true;
+  if (!window.Worker) {
+    browserIsSupported = false;
+    document.getElementById('noSupport').style.display = "block";
+    document.getElementById('noWorkerSupport').style.display = "block";
+  }
+  //http://www.voormedia.nl/blog/2012/10/displaying-and-detecting-support-for-svg-images
+  if (!document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Image", "1.1")) {
+    browserIsSupported = false;
+    document.getElementById('noSupport').style.display = "block";
+    document.getElementById('noSVGSupport').style.display = "block";
+  }
+  return browserIsSupported;
+};
+
+window.onload = function() {
+
+  if (!dp.browserIsSupported()) {
+    return;
+  }
+
+  dp.initializeControlValues();
+  document.getElementById('startPause').onclick = dp.startPause;
+  document.getElementById('reload').onclick = dp.loadNewDrawer;
+  var trailCheckbox = document.getElementById('trailCheckbox');
+  trailCheckbox.onclick = dp.setTrail;
+  dp.trailOn = trailCheckbox.checked;
+  document.getElementById("genSVG").onclick = dp.generateSVG;
+  document.getElementById('svgText').value = "";
+  var controlsHeight = document.getElementById('controls').offsetHeight;
+  var controlsHelp = document.getElementById('controlsHelp');
+  // heights are unkown until rendering, so here we are
+  controlsHelp.style.top = controlsHeight - 8 + "px";
+  document.getElementById('controlsHelpButton').onclick =
+    dp.elementDisplayer('controlsHelp');
+  document.getElementById('getSVGHelpButtonSVG').onclick =
+    dp.elementDisplayer('getSVGHelpText');
+  var getSVGHeight = document.getElementById('getSVGHeader').offsetHeight;
+  var getSVGHelpText = document.getElementById('getSVGHelpText');
+  getSVGHelpText.style.top = getSVGHeight + "px";
+  
+  dp.worker = new Worker('dpWorker.js');
+  // ideally we would be passing JSON data by reference (not copy), but
+  // word on the web is that IE10 still doesn't support even passing
+  // objects, much less by reference.  Another alternative is 
+  // encoding/parsing JSON strings, but that's slower than this and not
+  // much simpler.
+  dp.worker.onmessage = (function() {
+    return function(event) {
+
+      var message = event.data;
+      var controlChar = message.charAt(0);
+      if (controlChar === "d") { // "d"ebug
+        //console.log("debug: " + message);
+      }
+      else { // data
+        var splitIndex = message.indexOf('!');
+        var lastDataPoint = message.substring(0, splitIndex);
+        var data = message.substring(splitIndex + 1);
+        dp.dataStore.addData(data, lastDataPoint);
+      }
+    };
+  }());
+  dp.loadNewDrawer();
+};
+
+// http://stackoverflow.com/questions/18082/validate-numbers-in-javascript-isnumeric
+dp.isNum = function (n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+};
---
+++ dpWorker.js
@@ -0,0 +1,159 @@
+"use strict";
+var dp = {};
+
+dp.l1 = 100; // length of first pendulum
+dp.l2 = 100;
+dp.m1 = 1; // mass of first pendulum
+dp.m2 = 1;
+dp.dataStore = (function() {
+
+  ////// private
+  var dataString = ""; // the generated data as a string
+  // [theta1, theta2, omega1, omega2] arrays, either initial conditions
+  // or final conditions (i.e. last data point of dataString -
+  // remember dataString doesn't store omegas)
+  var initialPoint = [0, 0, 0, 0];
+  var finalPoint = [0, 0, 0, 0];
+  ////// public
+  return {
+    dataString : function() { return dataString; },
+    setDataString : function(s) { dataString = s; },
+    setInitialConditions : function(data) {
+    initialPoint[0] = parseFloat(data[0]);
+    initialPoint[1] = parseFloat(data[1]);
+    initialPoint[2] = parseFloat(data[2]);
+    initialPoint[3] = parseFloat(data[3]);
+    },
+    clearData : function() {
+      dataString = "";
+      initialPoint = [0, 0, 0, 0];
+      finalPoint = [0, 0, 0, 0];
+    },
+    initialData : function() {
+      return initialPoint;
+    },
+    setInitialData : function(data) {
+      initialPoint = data.slice();
+    },
+    finalData : function() {
+      return finalPoint;
+    },
+    setFinalData : function(data) {
+      finalPoint = data.slice();
+    }
+  };
+}());
+
+dp.setConstants = function(data) {
+
+  dp.l1 = parseFloat(data[0]);
+  dp.l2 = parseFloat(data[1]);
+  dp.m1 = parseFloat(data[2]);
+  dp.m2 = parseFloat(data[3]);
+};
+
+dp.generateData = function(steps) {
+
+  var h = 0.005; // time step
+  var l1 = dp.l1;
+  var l2 = dp.l2;
+  var m1 = dp.m1;
+  var m2 = dp.m2;
+  var M = m1 + m2;
+  //var g = 9.80665;-->hard coded
+  // omega1 and omega2 formulas are from
+  // https://freddie.witherden.org/tools/doublependulum/
+  // I think some other sources on the web may have them wrong
+  // omega1 = (d/dt)theta1
+  var f3 = (function(m1, m2, M, l1, l2, sin, cos) {
+    return function(y1, y2, y3, y4) {
+      return (m2 * l1 * y3 * y3 * sin(y2 - y1) * cos(y2 - y1) + 
+              m2 * 9.80665 * sin(y2) * cos(y2 - y1) +
+              m2 * l2 * y4 * y4 * sin(y2 - y1) - M * 9.80665 * sin(y1)) /
+        (M * l1 - m2 * l1 * cos(y2 - y1) * cos(y2 - y1));
+    };
+  }(m1, m2, M, l1, l2, Math.sin, Math.cos));
+  // omega2 = (d/dt)theta2
+  var f4 = (function(m1, m2, M, l1, l2, sin, cos) {
+    return function(y1, y2, y3, y4) {
+      return (-m2 * l2 * y4 * y4 * sin(y2 - y1) * cos(y2 - y1) +
+              M * 9.80665 * sin(y1) * cos(y2 - y1) -
+              M * l1 * y3 * y3 * sin(y2 - y1) - M * 9.80665 * sin(y2)) /
+        (M * l2 - m2 * l2 * cos(y2 - y1) * cos(y2 - y1));
+    };
+  }(m1, m2, M, l1, l2, Math.sin, Math.cos));
+  var cur = dp.dataStore.initialData();
+  // start the data run - the final result will be a |-separated
+  // list of comma-separated x,y values
+  var data = cur[0] + ',' + cur[1];
+  // we only actually report every keepStepModulus'th data point
+  var keepStepModulus = 40;
+  // for simplicity we need to end on a step that we keep (so that cur
+  // actually comes out as the final data point at the end of the
+  // loop)
+  steps = steps * keepStepModulus + 1;
+  var i;
+  for (i = 0; i < steps; ++i) {
+
+    var y1n = cur[0]; // theta1
+    var y2n = cur[1]; // theta2
+    var y3n = cur[2]; // omega1 = dtheta1/dt
+    var y4n = cur[3]; // omega2 = dtheta2/dt
+    // Compute the next iteration "y_{n+1}" using classical Runge-Kutta
+    var a1 = h * y3n;
+    var a2 = h * y4n;
+    var a3 = h * f3(y1n, y2n, y3n, y4n);
+    var a4 = h * f4(y1n, y2n, y3n, y4n);
+    var b1 = h * (y3n + 0.5 * a3);
+    var b2 = h * (y4n + 0.5 * a4);
+    var b3 = h * f3(y1n + 0.5 * a1, y2n + 0.5 * a2,
+                  y3n + 0.5 * a3, y4n + 0.5 * a4);
+    var b4 = h * f4(y1n + 0.5 * a1, y2n + 0.5 * a2,
+                  y3n + 0.5 * a3, y4n + 0.5 * a4);
+    var c1 = h * (y3n + 0.5 * b3);
+    var c2 = h * (y4n + 0.5 * b4);
+    var c3 = h * f3(y1n + 0.5 * b1, y2n + 0.5 * b2,
+                  y3n + 0.5 * b3, y4n + 0.5 * b4);
+    var c4 = h * f4(y1n + 0.5 * b1, y2n + 0.5 * b2,
+                  y3n + 0.5 * b3, y4n + 0.5 * b4);
+    var d1 = h * (y3n + c3);
+    var d2 = h * (y4n + c4);
+    var d3 = h * f3(y1n + c1, y2n + c2, y3n + c3, y4n + c4);
+    var d4 = h * f4(y1n + c1, y2n + c2, y3n + c3, y4n + c4);
+    var y1new = y1n + a1 / 6 + b1 / 3 + c1 / 3 + d1 / 6;
+    var y2new = y2n + a2 / 6 + b2 / 3 + c2 / 3 + d2 / 6;
+    var y3new = y3n + a3 / 6 + b3 / 3 + c3 / 3 + d3 / 6;
+    var y4new = y4n + a4 / 6 + b4 / 3 + c4 / 3 + d4 / 6;
+    // save for the next iteration
+    cur = [y1new, y2new, y3new, y4new];
+    if (i % keepStepModulus === 0) {
+      data += '|' + y1new + ',' + y2new;
+    }
+  }
+  dp.dataStore.setDataString(data);
+  dp.dataStore.setFinalData(cur);
+};
+
+onmessage = function(event) {
+
+  var message = event.data;
+  var controlChar = message.charAt(0);
+  message = message.substring(1);
+  if (controlChar === 'c') { // 'c'onfig
+    var dataArray = message.split('|');
+    dp.setConstants(dataArray);
+  }
+  else if (controlChar === 's') { // 's'tart data generation
+    dp.dataStore.clearData();
+    var parts = message.split('!');
+    var numDatapointsRequested = parts[0];
+    var initialConditions = parts[1].split('|');
+    dp.dataStore.setInitialConditions(initialConditions);
+    dp.generateData(numDatapointsRequested);
+    // now return the data
+    var lastDataPoint = dp.dataStore.finalData();
+    var lastDataString = lastDataPoint.join('|');
+    postMessage(lastDataString + '!' + dp.dataStore.dataString());
+    dp.dataStore.clearData();
+  }
+};
